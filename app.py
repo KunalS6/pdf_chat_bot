55f489f670509aadc2d83aed7d2468869a1ad1b2
KunalS6 <samantakunal6@gmail.com>
2026-02-11 21:34:51 +0530

    changes in the langachin chains

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-'''
 # =========================
 # Streamlit UI
 # =========================
@@ -326,18 +325,5 @@ if st.session_state.rag_chain:
 else:
     st.info("⬆️ Upload a PDF to begin")
 
-'''
-
-
-
-from groq import Groq
-
-client = Groq(api_key="gsk_your_real_groq_key_here")
-
-resp = client.chat.completions.create(
-    model="llama-3.1-8b-instant",
-    messages=[{"role": "user", "content": "Hello"}],
-)
-print(resp.choices[0].message.content)
 
 
